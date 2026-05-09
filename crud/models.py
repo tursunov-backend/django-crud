067309daeb7ec9da2bdb7dbaf61a959560f0c11d
djumanov <djumanovdev@gmail.com>
2026-05-09 10:24:38 +0500

    model

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=127)
+    description = models.TextField(default='', null=False, blank=True)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+
+
+    def __str__(self):
+        return self.name
+    
